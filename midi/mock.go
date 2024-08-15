111cb158952e5e0cca5230926f3fbc98a223d639
Xavier Godart <xavier.godart64@gmail.com>
2024-08-15 15:47:48 +0200

    fix midi mock

--- midi/mock.go
+++ midi/mock.go
@@ -2,17 +2,21 @@ package midi
 
 import (
 	gomidi "gitlab.com/gomidi/midi/v2"
+	"gitlab.com/gomidi/midi/v2/drivers"
 )
 
 type Mock struct{}
 
-func (m *Mock) Devices() gomidi.OutPorts                                     { return nil }
-func (m *Mock) NoteOn(device int, channel uint8, note uint8, velocity uint8) {}
-func (m *Mock) NoteOff(device int, channel uint8, note uint8)                {}
-func (m *Mock) Silence(device int, channel uint8)                            {}
-func (m *Mock) ControlChange(device int, channel, controller, value uint8)   {}
-func (m *Mock) ProgramChange(device int, channel uint8, value uint8)         {}
-func (m *Mock) Pitchbend(device int, channel uint8, value int16)             {}
-func (m *Mock) AfterTouch(device int, channel uint8, value uint8)            {}
-func (m *Mock) SendClock(devices []int)                                      {}
-func (m *Mock) Close()                                                       {}
+func (m *Mock) Devices() gomidi.OutPorts                         { return nil }
+func (m *Mock) ActiveDevice() drivers.Out                        { return nil }
+func (m *Mock) SetActiveDevice(device int)                       {}
+func (m *Mock) CycleMidiDevices()                                {}
+func (m *Mock) NoteOn(channel uint8, note uint8, velocity uint8) {}
+func (m *Mock) NoteOff(channel uint8, note uint8)                {}
+func (m *Mock) Silence(channel uint8)                            {}
+func (m *Mock) ControlChange(channel, controller, value uint8)   {}
+func (m *Mock) ProgramChange(channel uint8, value uint8)         {}
+func (m *Mock) Pitchbend(channel uint8, value int16)             {}
+func (m *Mock) AfterTouch(channel uint8, value uint8)            {}
+func (m *Mock) SendClock()                                       {}
+func (m *Mock) Close()                                           {}
