2336330a9cb5a196edba3d164f093e6d90d683b7
Xavier Godart <xavier.godart64@gmail.com>
2024-11-30 15:56:01 +0100

    refactore meta commands

--- core/music/meta/commad.go
+++ core/music/meta/commad.go
@@ -2,13 +2,6 @@ package meta
 
 import (
 	"signls/core/common"
-	"signls/midi"
-)
-
-const (
-	defaultKey = 60 // Middle C
-	maxKey     = 127
-	minKey     = 21
 )
 
 type Command interface {
@@ -20,55 +13,4 @@ type Command interface {
 	Value() *common.ControlValue[int]
 	Display() string
 	Reset()
-}
-
-type RootCommand struct {
-	value    *common.ControlValue[int]
-	executed bool
-	active   bool
-}
-
-func NewRootCommand() *RootCommand {
-	return &RootCommand{
-		value: common.NewControlValue[int](defaultKey, minKey, maxKey),
-	}
-}
-
-func (c *RootCommand) Copy() Command {
-	newValue := *c.value
-	return &RootCommand{
-		value:  &newValue,
-		active: c.active,
-	}
-}
-
-func (c *RootCommand) Active() bool {
-	return c.active
-}
-
-func (c *RootCommand) SetActive(active bool) {
-	c.active = active
-}
-
-func (c *RootCommand) Executed() bool {
-	return c.executed
-}
-
-func (c *RootCommand) Execute() {
-	if !c.active {
-		return
-	}
-	c.executed = true
-}
-
-func (c *RootCommand) Value() *common.ControlValue[int] {
-	return c.value
-}
-
-func (c *RootCommand) Display() string {
-	return midi.Note(uint8(c.value.Value()))
-}
-
-func (c *RootCommand) Reset() {
-	c.executed = false
 }
